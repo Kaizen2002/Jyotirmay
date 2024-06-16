c2bd2fae32d2f1f69e62670424983ad0a2de4f9b
Kaizen <jyotirmay231@gmail.com>
2024-06-16 15:59:41 +0530

    Try to deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/jyotirmay', // Replace 'jyotirmay' with your repository name
+  base: '/', // Replace 'jyotirmay' with your repository name
   plugins: [react()],
 });
